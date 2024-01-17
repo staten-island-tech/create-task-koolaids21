90a3a38acf09c6d5a9201a791b7d1ef68040c95d
weihong <weihong2398@gmail.com>
2024-01-17 13:15:29 -0500

    pppp

--- project/js/main.js
+++ project/js/main.js
@@ -1,24 +1,20 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from '../counter.js'
+  import {troops} from '../js/card.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const Domselectors = {
+  troop: document.querySelector("#app"),
+};
 
-setupCounter(document.querySelector('#counter'))
+Domselectors.troop.addEventListener("click", () =>{   //click this button to generate a random card that you should use in your next deck
+function Algorithm(x) {
+  Domselectors.troop.innerHTML = "";
+  x.forEach((agent) => {
+    Domselectors.troop.insertAdjacentHTML(
+      "beforeend",
+      ` <div class="card">
+        </div>`
+    );
+  });
+}}
+)
+
+troops.forEach();
