51ac41c566fae3320935ee596906c5c52989c783
weihong <weihong2398@gmail.com>
2024-01-30 13:17:24 -0500

    zzzz

--- project/js/main.js
+++ project/js/main.js
@@ -1,5 +1,9 @@
 import { troops } from "../js/card.js";
 
+const Domselectors = {
+  CARDDISPLAY: document.getElementById("cardDisplay"),
+}
+
 function generatecard() {
   // function generates a card by selecting a random array inside the big array troops by using math random.
   const randomIndex = Math.floor(Math.random() * troops.length);
@@ -7,13 +11,13 @@ function generatecard() {
 }
 function generatenewcard() {
   // function will generate a new card when the button is pressed
-  const carddisplay = document.getElementById("cardDisplay");
   const randomCard = generatecard();
-  carddisplay.textContent =
-    "Generated Card: " +
-    randomCard.name +
-    " Description: " +
-    randomCard.description;
+  Domselectors.CARDDISPLAY.innerHTML=
+      `Generated Card: 
+      ${randomCard.name}
+       Description:
+      ${randomCard.description}`
+  
 }
 const generateButton = document.getElementById("generateButton");
 generateButton.addEventListener("click", generatenewcard);
