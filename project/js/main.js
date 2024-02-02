7c37d59ea874da3405b857ce6558d88f77ab150a
koolaids21 <weihong2398@gmail.com>
2024-02-01 23:32:36 -0500

    aaaa

--- project/js/main.js
+++ project/js/main.js
@@ -2,9 +2,8 @@ import { troops } from "../js/card.js";
 
 const Domselectors = {
   CARDDISPLAY: document.getElementById("cardDisplay"),
-  PreviouslyGeneratedCard: document.getElementById("generatedcards"),
   generatebutton: document.getElementById("generateButton"),
-}
+};
 
 function generatecard() {
   // function generates a card by selecting a random array inside the big array troops by using math random.
@@ -12,24 +11,32 @@ function generatecard() {
   return troops[randomIndex];
 }
 
+Domselectors.generatebutton.addEventListener("click", generatenew);
+
 function generatenew() {
   // function will generate a new card when the button is pressed
   const randomCard = generatecard();
-  Domselectors.CARDDISPLAY.insertAdjacentHTML=
-      `
+  Domselectors.CARDDISPLAY.insertAdjacentHTML(
+    "beforeend",
+    `
       <div class ="card">
       <p>Generated Card: 
       ${randomCard.name}
        Description:
       ${randomCard.description}<p/>
       </div>`
-  
+  );
+  cardlimit();
 }
-generateButton.addEventListener("click", generatenew);
 
+function cardlimit() {
+  const carddisplayed = Domselectors.CARDDISPLAY.querySelectorAll(".card");
+  const cardAmount = carddisplayed.length;
 
-for (let i = 0; i < troops.length; i++){
-  if( i === 30 ){
-
+  if (cardAmount > 8) {
+    const extracards = cardAmount - 8;
+    for (let i = 0; i < extracards; i++) {
+      Domselectors.CARDDISPLAY.removeChild(carddisplayed[i]);
+    }
   }
 }
